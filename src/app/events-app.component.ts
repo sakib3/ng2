4c15bdafd826d0eec9ec093b0832c9f6125d94b6
Sabbir <sabbir.rahman.sakib@teleopti.com>
2018-07-16 16:01:37 +0200

    Creating Your First Data-bound Component

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'events-app',
   template: `
-  <h1>Hello World</h1>
-  <img src="/assets/images/ng.png">
+  <events-list></events-list>
   `
 })
 export class EventsAppComponent {
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'events-list',
+    template: `
+    <div>
+        <h1>Upcomming Events</h1>
+        <hr>
+        <div class="well hoverwell thumbnail">
+            <h2>{{event.name}}</h2>
+            <div>Date : {{event.date}}</div>
+            <div>Time : {{event.time}}</div>
+            <div>Price : \${{event.price}}</div>
+            <div>
+                <span>Location : {{event.location.address}}</span>
+                <span>&nbsp;</span>
+                <span>{{event.location.city}}, {{event.location.country}}</span>
+            </div>
+        </div>
+    </div>
+    `
+})
+export class EventListComponent {
+    event = {
+        id: 1,
+        name:'Angular Connect',
+        date: '9/26/2036',
+        time: '10:00 am',
+        price: 599.99,
+        imageUrl: '/assets/images/ng-conf.jpg',
+        location: {
+            address: '1057 DT',
+            city: 'London',
+            country: 'England'
+        }
+    }
+}
