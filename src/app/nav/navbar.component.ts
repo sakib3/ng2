354945d749dea231c99187816d074c4f2a51eb5c
Sabbir <sabbir.rahman.sakib@teleopti.com>
2018-08-02 15:45:29 +0200

    styling active links

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'nav-bar',
-    templateUrl: './navbar.component.html'
+    templateUrl: './navbar.component.html',
+    styles:[`
+        li > a.active {color: #F97924;}
+    `]
 })
 export class NavbarComponent {
 }
