42f88ed52e2dc1a9e2dee7d817eeebbdc2ad1a44
Sabbir <sabbir.rahman.sakib@teleopti.com>
2018-08-02 15:35:02 +0200

    pre loaded data for component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { RouterModule } from '../../node_modules/@angular/router';
 import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
 import { EventRouteActivator } from './events/event-details/event-route-activator.service';
+import { EventListResolver } from './events/events-list-resolver.service';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { EventRouteActivator } from './events/event-details/event-route-activato
     EventService, 
     ToastrService, 
     EventRouteActivator,
+    EventListResolver,
     { provide:'canDeactivateCreateEvent', useValue: checkDirtyState }
   ],
   bootstrap: [EventsAppComponent]
