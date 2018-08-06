e4fce278f2490df7472bf42ad4381cd7ec6e96d7
Sabbir <sabbir.rahman.sakib@teleopti.com>
2018-08-06 16:51:51 +0200

    lazyloading feature module

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -11,5 +11,6 @@ export const appRoutes:Routes = [
     {path:'events/new', component: CreateEventComponent, canDeactivate:['canDeactivateCreateEvent']},
     {path:'events', component: EventListComponent, resolve:{events: EventListResolver}},
     {path:'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
-    {path:'', redirectTo: '/events', pathMatch: 'full'}
+    {path:'', redirectTo: '/events', pathMatch: 'full'},
+    {path:'user', loadChildren:'./user/user.module#UserModule'}
 ];
