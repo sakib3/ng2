d7a93bfbf099f1a3df3e9adef7cebc808195cd6d
Sabbir <sabbir.rahman.sakib@teleopti.com>
2018-08-24 16:31:12 +0200

    using models for type safety

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,25 +1,26 @@
 import { Injectable } from "@angular/core";
-import { Subject} from 'rxjs';
+import { Subject, Observable} from 'rxjs';
+import { IEvent } from "./event.model";
 
 @Injectable() 
 export class EventService {
-    getEvents(){
-        let subject = new Subject();
+    getEvents(): Observable<IEvent[]>{
+        let subject = new Subject<IEvent[]>();
         setTimeout(() => { 
             subject.next(EVENTS);
             subject.complete();
         } ,2000);
         return subject;
     }
-    getEvent = (id: number) => EVENTS.find((e) => e.id == id);
+    getEvent = (id: number):IEvent => EVENTS.find((e) => e.id == id);
 }
 
 
-const EVENTS = [
+const EVENTS: IEvent[] = [
     {
         id: 1,
         name:'Angular Connect',
-        date: '9/26/2036',
+        date: new Date('9/26/2036'),
         time: '10:00 am',
         price: 599.99,
         imageUrl: '/assets/images/ng-conf.jpg',
@@ -32,7 +33,7 @@ const EVENTS = [
     {
         id: 2,
         name:'React Connect',
-        date: '7/13/2116',
+        date: new Date('7/13/2116'),
         time: '11:00 am',
         price: 299.99,
         imageUrl: '/assets/images/react-conf.png',
@@ -45,7 +46,7 @@ const EVENTS = [
     {
         id: 3,
         name:'Vue Connect',
-        date: '1/10/2046',
+        date: new Date('1/10/2046'),
         time: '08:00 am',
         price: 899.99,
         imageUrl: '/assets/images/vue-conf.png',
