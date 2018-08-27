cdfaab6a9eecc26189c753a0961d20d085aa8096
Sabbir <sabbir.rahman.sakib@teleopti.com>
2018-08-27 16:57:29 +0200

    Creating First Template-based Form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
+import { FormsModule} from "@angular/forms";
 import {userRoutes} from "./user.routes"
 import { ProfileComponent } from "./profile.component";
+import { LoginComponent } from "./login.component";
+
+
 @NgModule({
-imports:[CommonModule, RouterModule.forChild(userRoutes)],
-declarations:[ProfileComponent],
+imports:[
+    CommonModule,
+    FormsModule, 
+    RouterModule.forChild(userRoutes)],
+declarations:[ProfileComponent, LoginComponent],
 providers:[]
 })
+
+
 export class UserModule{}
